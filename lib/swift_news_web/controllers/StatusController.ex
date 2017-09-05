45285effb8d1f113e376a8a1abf724291a8b3291
pmick <patrickmick1@gmail.com>
2017-09-04 19:02:32 -0700

    Cleaning up endpoint code for basic status endpoint

--- lib/swift_news_web/controllers/StatusController.ex
+++ lib/swift_news_web/controllers/StatusController.ex
@@ -3,6 +3,6 @@ defmodule SwiftNewsWeb.StatusController do
 
     def index(conn, _params) do
         conn |>
-            Phoenix.Controller.json(%{"version": "1.0.0"})
+            json(%{"version": "1.0.0"})
     end
 end
