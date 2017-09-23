3a12e49bd4a38afbb1d995a775fe3ce24a2e49f5
pmick <patrickmick1@gmail.com>
2017-09-23 14:39:35 -0700

    Removing now unused hand written auth code

--- lib/swift_news_web/router.ex
+++ lib/swift_news_web/router.ex
@@ -11,7 +11,7 @@ defmodule SwiftNewsWeb.Router do
     plug Guardian.Plug.VerifySession
     plug Guardian.Plug.VerifyHeader, realm: "Bearer"
     plug Guardian.Plug.LoadResource
-    plug Guardian.Plug.EnsureAuthenticated, handler: SwiftNews.AuthErrorHandler 
+    plug Guardian.Plug.EnsureAuthenticated
   end
 
   scope "/", SwiftNewsWeb do
@@ -27,19 +27,5 @@ defmodule SwiftNewsWeb.Router do
     pipe_through [:api, :authenticated_api]
 
     post "/story/", StoryController, :create    
-  end
-
-  def user_authenticated(conn, _) do
-    token = SwiftNews.Guardian.Plug.current_token(conn)
-    case SwiftNews.Guardian.decode_and_verify(token) do
-      {:ok, _} ->
-        conn
-      {:error, _} ->
-        conn
-        |> put_status(401)
-        |> json("Invalid auth token")
-        |> halt
-      
-    end
   end
 end
