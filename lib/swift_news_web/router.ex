191c18e8aeb52ad47e3eb4461659d91d4c77a81f
pmick <patrickmick1@gmail.com>
2017-09-23 13:35:29 -0700

    Making story creation require auth

--- lib/swift_news_web/router.ex
+++ lib/swift_news_web/router.ex
@@ -5,6 +5,11 @@ defmodule SwiftNewsWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :authenticated_api do
+    plug :accepts, ["json"]
+    plug :user_authenticated
+  end
+
   scope "/", SwiftNewsWeb do
     pipe_through :api
 
@@ -13,6 +18,25 @@ defmodule SwiftNewsWeb.Router do
     post "/user/", AuthController, :create
     post "/auth/", AuthController, :login
 
-    post "/story/", StoryController, :create
+  end
+
+  scope "/", SwiftNewsWeb do
+    pipe_through :authenticated_api
+
+    post "/story/", StoryController, :create    
+  end
+
+  def user_authenticated(conn, _) do
+    token = SwiftNews.Guardian.Plug.current_token(conn)
+    case SwiftNews.Guardian.decode_and_verify(token) do
+      {:ok, _} ->
+        conn
+      {:error, _} ->
+        conn
+        |> put_status(401)
+        |> json("Invalid auth token")
+        |> halt
+      
+    end
   end
 end
